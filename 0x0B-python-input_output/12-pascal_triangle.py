715244c25c2f9825a15cf53937812ed98d0e3c44
Robe <47053081+robegetachew@users.noreply.github.com>
2022-08-02 15:38:49 +0300

    12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
-"""Module 14-pascal_triangle.
-Returns a list of lists of integers
-representing the Pascal’s triangle of n.
+"""Pascal's triangle
 """
 
 
 def pascal_triangle(n):
-    """Returns the pascal triangle of n.
-    Args:
-        - n: size of the triangle (rows)
-    Returns: a list of list of integers
-    """
-
+    """returns a list of intergers representing the
+    pascal's triangle of n"""
     if n <= 0:
         return []
-
-    l = [[0 for x in range(i + 1)] for i in range(n)]
-    l[0] = [1]
-
-    for i in range(1, n):
-        l[i][0] = 1
-        for j in range(1, i + 1):
-            if j < len(l[i - 1]):
-                l[i][j] = l[i - 1][j - 1] + l[i - 1][j]
-            else:
-                l[i][j] = l[i - 1][0]
-    return l
+    a = [[] for i in range(n)]
+    for i in range(n):
+        for j in range(i + 1):
+            if j < i:
+                if j == 0:
+                    a[i].append(1)
+                else:
+                    a[i].append(a[i - 1][j] + a[i - 1][j - 1])
+            elif j == i:
+                a[i].append(1)
+    return a
